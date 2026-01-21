8e883e94451e8278ec8e44a37ecc36ec4bd8ec93
ian benia <ianbenia03@gmail.com>
2026-01-21 20:09:39 -0300

    Projeto inicial - Fridge API

---
+++ Fridge/src/main/java/dev/java10x/Fridge/model/Food.java
@@ -0,0 +1,56 @@
+package dev.java10x.Fridge.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "food_table")
+public class Food {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer quantity;
+    private LocalDateTime expirationDate;
+
+    public Food() {
+
+    }
+
+    public Food(Long id, String name, Integer quantity, LocalDateTime expirationDate) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.expirationDate = expirationDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public LocalDateTime getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setExpirationDate(LocalDateTime expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+}
---
+++ Fridge/src/main/java/dev/java10x/Fridge/service/FoodService.java
@@ -0,0 +1,20 @@
+package dev.java10x.Fridge.service;
+
+import dev.java10x.Fridge.model.Food;
+import dev.java10x.Fridge.repository.FoodRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class FoodService {
+
+    private final FoodRepository foodRepository;
+
+    public FoodService(FoodRepository foodRepository) {
+        this.foodRepository = foodRepository;
+    }
+
+    public List<Food> getAll() {return foodRepository.findAll();}
+    public Food save(Food food) {return foodRepository.save(food);}
+    public void delete(Long id){foodRepository.deleteById(id);}
+}
